6a0695b726d6912103e47c6d907e0eb7e71391b3
Todd Doughty <tdoughty1@berkeley.edu>
2013-12-29 12:08:10 -0800

    Added ship base classes, Frigate module

--- Military/Navy/BaseClasses.py
+++ Military/Navy/BaseClasses.py
@@ -3,3 +3,17 @@ from Imperium.Military.BaseClasses import Unit
 
 class Ship(Unit):
     pass
+
+
+class ShipClass(DObject):
+    pass
+
+
+class ShipType(DObject):
+
+    def _init__(self, name, code=None):
+        DOject.__init__(self, name)
+        self._SetCode(code)
+    
+    def _SetCode(self, code):
+        self._code = code
