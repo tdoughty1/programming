8206369af35f609285d99455483ff547a3f1a111
Todd Doughty <tdoughty84@berkeley.edu>
2014-04-17 10:05:53 -0700

    First commit for various record classes used in reading file

---
+++ cdmsbats/histsubrecord.h
@@ -0,0 +1,27 @@
+#ifndef HISTSUBRECORD_H
+#define HISTSUBRECORD_H
+
+class HistSubRecord : public DataRecord
+{
+    public:
+        /** Default constructor */
+        HistSubRecord();
+
+        /** Default destructor */
+        virtual ~HistSubRecord(){};
+
+        void ReadRecord(CDMSRawFileStream*, int, bool);
+
+        void StoreValues(int32_t*){};
+
+        void PrintValues(){};
+
+    protected:
+    private:
+
+        BookRecord* _BookPtr;
+        TimeRecord* _TimePtr;
+        uint* _Trace[];
+};
+
+#endif // HISTSUBRECORD_H
