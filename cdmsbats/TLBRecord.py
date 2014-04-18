4decd78294b9990e04391042412bb8f925bad1c1
Todd Doughty <tdoughty84@berkeley.edu>
2014-04-17 17:10:42 -0700

    Found error in reading TLB record

--- cdmsbats/TLBRecord.py
+++ cdmsbats/TLBRecord.py
@@ -13,16 +13,13 @@ class TLBRecord(DataRecord):
 
     def __init__(self):
 
-        self._TriggerTime = None
         self._TriggerMask = None
 
     def StoreValues(self, Record):
 
-        self._TriggerTime = Record[0]
-        self._TriggerMask = copy(Record[1:7])
+        self._TriggerMask = copy(Record[:])
 
     def PrintValues(self):
 
-        print "Trigger Time = %d" % self._TriggerTime
         for i, mask in zip(range(len(self._TriggerMask)), self._TriggerMask):
             print "Tower %d TLB Mask = 0x%x" % (i+1, mask)
