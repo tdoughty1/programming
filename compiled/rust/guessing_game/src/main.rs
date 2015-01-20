3d49bfb8b865cac13b839f6806a455cd3660bd11
Todd Doughty <tdoughty1@berkeley.edu>
2015-01-19 20:31:22 -0800

    Added comparison, input type setting

--- compiled/rust/guessing_game/src/main.rs
+++ compiled/rust/guessing_game/src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
 use std::rand;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
     
-    let secret_number = (rand::random::<i32>() % 100) + 1; // secret number: i32
+    let secret_number = (rand::random::<u32>() % 100) + 1; // secret number: i32
     
     println!("The secret number is: {}", secret_number);
     
@@ -14,5 +15,28 @@ fn main() {
                            .ok()
                            .expect("Failed to read line");
 
-    println!("You guessed: {}", input);
+    let input_num: Option<u32> = input.parse();
+    
+    let num = match input_num {
+        Some(num) => num,
+        None => {
+            println!("Please input a number!");
+            return;
+        }
+    };
+
+    println!("You guessed: {}", num);
+    
+    match cmp(num, secret_number){
+        Ordering::Less => println!("Guess is too low!"),
+        Ordering::Greater => println!("Guess is too high!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
+
+fn cmp(a: u32, b: u32) -> Ordering{
+    if a < b {Ordering::Less}
+    else if a > b {Ordering::Greater}
+    else {Ordering::Equal}
+}
+
