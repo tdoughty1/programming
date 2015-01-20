e3f31d51a307f7778f95f338ed1081932bb14db3
Todd Doughty <tdoughty1@berkeley.edu>
2015-01-19 20:38:45 -0800

    Completed game

--- compiled/rust/guessing_game/src/main.rs
+++ compiled/rust/guessing_game/src/main.rs
@@ -4,33 +4,37 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = (rand::random::<u32>() % 100) + 1; // secret number: i32
-    
-    println!("The secret number is: {}", secret_number);
-    
-    println!("Please input your guess.");
-    
-    let input = io::stdin().read_line()
-                           .ok()
-                           .expect("Failed to read line");
 
-    let input_num: Option<u32> = input.parse();
-    
-    let num = match input_num {
-        Some(num) => num,
-        None => {
-            println!("Please input a number!");
-            return;
+    loop {
+
+        println!("Please input your guess.");
+
+        let input = io::stdin().read_line()
+                               .ok()
+                               .expect("Failed to read line");
+
+        let input_num: Option<u32> = input.trim().parse();
+
+        let num = match input_num {
+            Some(num) => num,
+            None => {
+                println!("Please input a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", num);
+
+        match cmp(num, secret_number){
+            Ordering::Less => println!("Guess is too low!"),
+            Ordering::Greater => println!("Guess is too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                return;
+            },
         }
-    };
-
-    println!("You guessed: {}", num);
-    
-    match cmp(num, secret_number){
-        Ordering::Less => println!("Guess is too low!"),
-        Ordering::Greater => println!("Guess is too high!"),
-        Ordering::Equal => println!("You win!"),
     }
 }
 
