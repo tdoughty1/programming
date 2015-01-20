c6e863b7d04fc7a76cdf734cb5c2719fa6db5fad
Todd Doughty <tdoughty1@berkeley.edu>
2015-01-19 20:09:19 -0800

    Added random number generator

--- compiled/rust/guessing_game/src/main.rs
+++ compiled/rust/guessing_game/src/main.rs
@@ -1,13 +1,16 @@
 use std::io;
+use std::rand;
 
 fn main() {
     println!("Guess the number!");
     
+    let secret_number = (rand::random::<i32>() % 100) + 1; // secret number: i32
+    
     println!("Please input your guess.");
     
     let input = io::stdin().read_line()
-                           .ok
+                           .ok()
                            .expect("Failed to read line");
 
-    println!("You guessed: {}", input");
+    println!("You guessed: {}", input);
 }
