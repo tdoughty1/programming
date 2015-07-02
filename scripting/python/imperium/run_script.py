ee2f76196bdb84e974dc02899e4fe24aeea73b11
Todd Doughty <tdoughty1@berkeley.edu>
2015-07-02 14:01:50 -0700

    Added encoding and bash header to call script, removed unnecessary import

--- scripting/python/imperium/run_script.py
+++ scripting/python/imperium/run_script.py
@@ -1,5 +1,8 @@
-from imperium.military.structure_classes import Branch, RankCategory, Rank
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
 from imperium.military.corps.light_cruiser import AugmentedPlatoonDet_CL
+
 
 def Main():
 	
