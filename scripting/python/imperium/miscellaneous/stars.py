2703c9a70592372d0785e7580dcd13d13d3ddfe1
Todd Doughty <tdoughty1@berkeley.edu>
2015-07-02 14:48:47 -0700

    used shorter names for more obvious calculation

--- scripting/python/imperium/miscellaneous/stars.py
+++ scripting/python/imperium/miscellaneous/stars.py
@@ -29,19 +29,28 @@ class System(object):
 
     def __init__(self, loc, name=None):
 
-        self._x = loc[0]
-        self._y = loc[1]
-        self._z = loc[2]
+        x = loc[0]
+        y = loc[1]
+        z = loc[2]
 
-        self._r = np.sqrt(self._x**2 + self._y**2 + self._z**2)
-        self._phi = angle(atan(self._y/self._x)*180/pi)
-        self._theta = angle(atan(self._z/(self._x**2 + self._y**2))*180/pi)
+        r = np.sqrt(x**2 + y**2 + z**2)
+        phi = angle(atan(y / x) * 180 / pi)
+        theta = angle(atan(z / (x**2 + y**2)) * 180 / pi)
+
+        self._x = x
+        self._y = y
+        self._z = z
+
+        self._r = r
+        self._phi = phi
+        self._theta = theta
 
         self._name = name
         self._stars = []
 
     def RightAscension(self):
-        print '%02dh, %02dm, %4.2f' %(self._phi._h)
+        # TODO - Fix actual definition
+        print '%02dh, %02dm, %4.2f' % (self._phi, self._phi, self._phi)
 
     def Declination(self):
         pass
